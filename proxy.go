f3dad79df61eed65bea29f3b2f7cac372a82a760
chiyikj <cyi.cc@outlook.com>
2024-07-29 20:57:17 +0800

    Update proxy.go

--- proxy.go
+++ proxy.go
@@ -2,43 +2,33 @@ package cyi
 
 import "github.com/gorilla/websocket"
 
-func (cyi *Cyi) watch(id string, keys []string, conn *websocket.Conn, requestId string, joinId string) {
-	_conn, ok := cyi.connList.Load(id)
-	if ok {
-		connMap, _ := _conn.(map[string]*connKey)
-		if connMap[joinId] != nil {
+func (cyi *Cyi) watch(id string, keys []string, conn *websocket.Conn, requestId string) {
+	_conn, _ := cyi.connList.Load(id)
+	for _, key := range keys {
+		if cyi.channel[key] {
+			_conn.(connKey).subscribe[key] = true
+		} else {
 			for _, key := range keys {
 				if cyi.channel[key] {
-					connMap[joinId].subscribe[key] = true
-				} else {
-					for _, key := range keys {
-						if cyi.channel[key] {
-							delete(connMap[joinId].subscribe, key)
-						}
-					}
-					err := conn.WriteJSON([]any{
-						requestId, resultCallError("cyi: The server does not have a subscription for this key"),
-					})
-					if err != nil {
-						conn.Close()
-						return
-					}
-					return
+					delete(_conn.(connKey).subscribe, key)
 				}
 			}
+			err := conn.WriteJSON([]any{
+				requestId, resultCallError("cyi: The server does not have a subscription for this key"),
+			})
+			if err != nil {
+				conn.Close()
+				return
+			}
+			return
 		}
 	}
 }
 
-func (cyi *Cyi) delWatch(id string, keys []string, joinId string) {
-	_conn, ok := cyi.connList.Load(id)
-	if ok {
-		connMap, _ := _conn.(map[string]*connKey)
-		if connMap[joinId] != nil {
-			for _, key := range keys {
-				delete(connMap[joinId].subscribe, key)
-			}
-		}
+func (cyi *Cyi) delWatch(id string, keys []string) {
+	_conn, _ := cyi.connList.Load(id)
+	for _, key := range keys {
+		delete(_conn.(connKey).subscribe, key)
 	}
 }
 
@@ -48,19 +38,19 @@ func (cyi *Cyi) NewChannel(keys ...string) {
 	}
 }
 
-func (cyi *Cyi) Send(id string, key string, data Result) {
+func (cyi *Cyi) Send(id string, key string, data Result) bool {
 	conn, ok := cyi.connList.Load(id)
-	if ok {
-		connMap, _ := conn.(map[string]*connKey)
-		for _, _conn := range connMap {
-			if _conn.subscribe[key] {
-				err := _conn.ws.WriteJSON([]any{
-					key, data,
-				})
-				if err != nil {
-					_conn.ws.Close()
-				}
-			}
+	if ok && conn.(connKey).subscribe[key] {
+		err := conn.(connKey).ws.WriteJSON([]any{
+			key, data,
+		})
+		if err != nil {
+			conn.(connKey).ws.Close()
+			return false
+		} else {
+			return true
 		}
+	} else {
+		return false
 	}
 }
